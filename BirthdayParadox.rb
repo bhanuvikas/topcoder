b12171423af996d88b852b4fea3029e2ffe904d1
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2013-06-05 20:28:35 -0500

    clearer, starting Collision problem

--- BirthdayParadox.rb
+++ BirthdayParadox.rb
@@ -8,9 +8,8 @@ class BirthdayOdds
 
     until two_people_share_a_birthday_odds >= min_odds or people == days_in_year
       people += 1
-      factors = []
-      people.times { |n| factors << (days_in_year-n-0.0) / days_in_year }
-      nobody_shares_a_birthday_odds = factors.reduce :*
+      nobody_shares_a_birthday_odds = 1.0
+      people.times { |n| nobody_shares_a_birthday_odds *= ((days_in_year-n-0.0) / days_in_year) }
       two_people_share_a_birthday_odds = (1 - nobody_shares_a_birthday_odds) * 100
     end
 
---
+++ Collision.rb
@@ -0,0 +1,7 @@
+class Collision
+
+  def probability assigments, ids
+
+  end
+
+end
